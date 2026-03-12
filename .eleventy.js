c8529764066a44ab320705195cddf37b94443833
Leon Schreiber <leon.schreiber@tu-berlin.de>
2026-03-12 16:30:45 +0100

    use eleventy + nunjucks for pure html site generation

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = function (eleventyConfig) {
+   // Pass static assets through to _site without processing
+   eleventyConfig.addPassthroughCopy("*.css");
+   eleventyConfig.addPassthroughCopy("*.pdf");
+
+   eleventyConfig.addPassthroughCopy("images");
+   eleventyConfig.addPassthroughCopy("fonts");
+
+   return {
+      dir: {
+         input: ".",
+         output: "_site",
+         includes: "_includes",
+      },
+   };
+};
